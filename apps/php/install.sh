7f9c28410b675f3617ec177ce131357ff87f3b20
Marcel <40896559+hackthedev@users.noreply.github.com>
2025-10-10 23:09:11 +0200

    Update install.sh

--- apps/php/install.sh
+++ apps/php/install.sh
@@ -2,15 +2,10 @@
 
 # this is where you do your install logic
 
-sudo apt install ca-certificates apt-transport-https lsb-release gnupg curl nano unzip -y
-sudo curl -fsSL https://packages.sury.org/php/apt.gpg -o /usr/share/keyrings/php-archive-keyring.gpg
-sudo echo "deb [signed-by=/usr/share/keyrings/php-archive-keyring.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list
+apt install php8.3 php8.3-cli php8.3-common php8.3-curl php8.3-gd php8.3-intl php8.3-mbstring php8.3-mysql php8.3-opcache php8.3-readline php8.3-xml php8.3-xsl php8.3-zip php8.3-bz2 libapache2-mod-php8.3 -y
+a2enmod php8.3
+systemctl restart apache2
 
-sudo apt update
-
-apt install php8.2 php8.2-cli php8.2-common php8.2-curl php8.2-gd php8.2-intl php8.2-mbstring php8.2-mysql php8.2-opcache php8.2-readline php8.2-xml php8.2-xsl php8.2-zip php8.2-bz2 libapache2-mod-php8.2 -y
-sudo a2enmod php8.2
-sudo service apache2 restart
 
 echo "initra://install/done"
 echo "initra://ssh/close"
