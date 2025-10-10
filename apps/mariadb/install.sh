15ad415900350e4e8eba433e491d8d0c21394860
Marcel <40896559+hackthedev@users.noreply.github.com>
2025-10-11 00:37:12 +0200

    test

---
+++ apps/mariadb/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# get argument parser
+source <(wget -qO- https://raw.githubusercontent.com/hackthedev/initra-shipping/refs/heads/main/snippets/argument_parser.sh)
+
+apt install mariadb-server mariadb-client -y
+
+# set the name as variable
+rootPw="$(getArg "rootPassword" "$@")"
+
+# check if null
+if [[ -z "$rootPw" ]]; then
+  # no root password set for some reason
+elif
+  # root password was set so we try to change the pw
+  mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$rootPw';"
+fi
+
+echo "initra://install/done"
+echo "initra://ssh/close"
