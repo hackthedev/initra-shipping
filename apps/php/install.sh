48a9509f862c479d033bf1da5d61e83010166682
Marcel <40896559+hackthedev@users.noreply.github.com>
2025-10-10 23:03:48 +0200

    fix?

--- apps/php/install.sh
+++ apps/php/install.sh
@@ -2,6 +2,8 @@
 
 # this is where you do your install logic
 apt install php8.2 php8.2-cli php8.2-common php8.2-curl php8.2-gd php8.2-intl php8.2-mbstring php8.2-mysql php8.2-opcache php8.2-readline php8.2-xml php8.2-xsl php8.2-zip php8.2-bz2 libapache2-mod-php8.2 -y
+sudo a2enmod php8.2
+sudo service apache2 restart
 
 echo "initra://install/done"
 echo "initra://ssh/close"
