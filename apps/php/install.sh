7f08597b3fbbd14356bbe5a43332249eff85d108
Marcel <40896559+hackthedev@users.noreply.github.com>
2025-10-10 22:56:22 +0200

    added php app

---
+++ apps/php/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# this is where you do your install logic
+apt install php8.2 php8.2-cli php8.2-common php8.2-curl php8.2-gd php8.2-intl php8.2-mbstring php8.2-mysql php8.2-opcache php8.2-readline php8.2-xml php8.2-xsl php8.2-zip php8.2-bz2 libapache2-mod-php8.2 -y
+
+echo "initra://install/done"
+echo "initra://ssh/close"
