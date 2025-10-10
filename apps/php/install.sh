26aa814918a374e4903cf6b519d07fbb4186c8f2
Marcel <40896559+hackthedev@users.noreply.github.com>
2025-10-10 23:12:31 +0200

    Update install.sh

--- apps/php/install.sh
+++ apps/php/install.sh
@@ -2,10 +2,15 @@
 
 # this is where you do your install logic
 
-apt install php8.3 php8.3-cli php8.3-common php8.3-curl php8.3-gd php8.3-intl php8.3-mbstring php8.3-mysql php8.3-opcache php8.3-readline php8.3-xml php8.3-xsl php8.3-zip php8.3-bz2 libapache2-mod-php8.3 -y
-a2enmod php8.3
-systemctl restart apache2
+apt install -y apt-transport-https lsb-release ca-certificates curl gnupg
+curl -fsSL https://packages.sury.org/php/apt.gpg | gpg --dearmor -o /usr/share/keyrings/sury-php.gpg
+echo "deb [signed-by=/usr/share/keyrings/sury-php.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/sury-php.list
+apt update
 
+
+apt install -y php8.2 php8.2-cli php8.2-common php8.2-curl php8.2-gd php8.2-intl php8.2-mbstring php8.2-mysql php8.2-opcache php8.2-readline php8.2-xml php8.2-zip libapache2-mod-php8.2
+a2enmod php8.2
+systemctl restart apache2
 
 echo "initra://install/done"
 echo "initra://ssh/close"
