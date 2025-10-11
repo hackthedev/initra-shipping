678f1b85b77605a8ff077229d324a1103386e4a7
Marcel <40896559+hackthedev@users.noreply.github.com>
2025-10-11 02:35:28 +0200

    Update install.sh

--- apps/mariadb/install.sh
+++ apps/mariadb/install.sh
@@ -11,6 +11,7 @@ rootPw="$(getArg "rootPassword" "$@")"
 # check if null
 if [[ -z "$rootPw" ]]; then
   # no root password set for some reason
+  echo "No password supplied"
 else
   # root password was set so we try to change the pw
   mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$rootPw';"
